d870828e0d5183f1a7ec78c1dce0e96527166fc9
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-07-09 19:52:06 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloCSharp
+    {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string nome = "Marika"; //variabile inizializzata. Portata a costante
+            int eta = 33; //se dentro doppi apici diventa una stringa. Questo è un intero.
+
+            Console.WriteLine("Hello C#");
+            Console.WriteLine("Ciao io sono " + nome); //concateno le variabili
+            Console.WriteLine("ho " + eta +" anni");
+            Console.WriteLine(nome + " è il mio primo nome");
+            Console.Read();
+        }
+    }
+}
